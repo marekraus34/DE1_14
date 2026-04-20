83c47f580952278991b110c98d62e2431a74847b
PeterRafaelis <145664354+PeterRafaelis@users.noreply.github.com>
2026-04-20 19:29:16 +0200

    Add files via upload

---
+++ led_bar_tb.vhd
@@ -0,0 +1,88 @@
+-- Testbench automatically generated online
+-- at https://vhdl.lapinoo.net
+-- Generation date : Mon, 20 Apr 2026 17:21:40 GMT
+-- Request id : cfwk-fed377c2-69e660a46c4dc
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_led_bar is
+end tb_led_bar;
+
+architecture tb of tb_led_bar is
+
+    component led_bar
+        port (clk           : in std_logic;
+              rst           : in std_logic;
+              level_i       : in std_logic_vector (7 downto 0);
+              valid_i       : in std_logic;
+              peak_active_i : in std_logic;
+              led_o         : out std_logic_vector (15 downto 0));
+    end component;
+
+    signal clk           : std_logic;
+    signal rst           : std_logic;
+    signal level_i       : std_logic_vector (7 downto 0);
+    signal valid_i       : std_logic;
+    signal peak_active_i : std_logic;
+    signal led_o         : std_logic_vector (15 downto 0);
+
+    constant TbPeriod : time := 10 ns; -- ***EDIT*** Put right period here
+    signal TbClock : std_logic := '0';
+    signal TbSimEnded : std_logic := '0';
+
+begin
+
+    dut : led_bar
+    port map (clk           => clk,
+              rst           => rst,
+              level_i       => level_i,
+              valid_i       => valid_i,
+              peak_active_i => peak_active_i,
+              led_o         => led_o);
+
+    -- Clock generation
+    TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
+
+    -- ***EDIT*** Check that clk is really your main clock signal
+    clk <= TbClock;
+
+    stimuli : process
+begin
+    -- Inicializácia
+    level_i <= (others => '0');
+    valid_i <= '0';
+    peak_active_i <= '0';
+    rst <= '1';
+    wait for 50 ns; -- Reset počas viacerých cyklov hodín
+    
+    rst <= '0';
+    wait for 50 ns;
+
+    -- Test 1: Nízka úroveň (napr. 10)
+    level_i <= x"0A"; -- hexadecimálne 10
+    valid_i <= '1';
+    wait for 100 ns;
+    valid_i <= '0'; -- Deaktivujeme valid
+    wait for 100 ns;
+
+    -- Test 2: Vysoká úroveň (napr. 200)
+    level_i <= x"C8"; -- hexadecimálne 200
+    valid_i <= '1';
+    wait for 100 ns;
+    valid_i <= '0';
+
+    -- Koniec simulácie
+    wait for 500 ns;
+    TbSimEnded <= '1';
+    wait;
+end process;
+
+end tb;
+
+-- Configuration block below is required by some simulators. Usually no need to edit.
+
+configuration cfg_tb_led_bar of tb_led_bar is
+    for tb
+    end for;
+end cfg_tb_led_bar;
